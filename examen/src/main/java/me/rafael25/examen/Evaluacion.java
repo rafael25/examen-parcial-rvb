d4b820dbb9df2d1750cc420bf6eaec3c7295055f
rafael25 <rafaelvb25@gmail.com>
2014-10-31 20:16:59 -0600

    Ejercicio 1

---
+++ examen/src/main/java/me/rafael25/examen/Evaluacion.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.rafael25.examen;
+
+/**
+ *
+ * @author T107
+ */
+public class Evaluacion {
+    String nombre;
+    float calificacion;
+
+    public Evaluacion(String nombre, float calificacion) {
+        this.nombre = nombre;
+        this.calificacion = calificacion;
+    }
+}
